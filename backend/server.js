44d3d45778e5588b1933afd658aadbc37472382c
yyf9991715 <yyf9991715@gmail.com>
2023-06-26 18:24:58 +0200

    update Server.js
    update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,30 @@
 const express = require("express");
-const cors = require("cors");
-const booksModel = require("./model/dbbookSchema.js");
-const userModel = require("./dbuserSchema")
-// Define port
-const port = 4000;
+//const cors = require("cors");
+//const booksModel = require("./model/dbbookSchema.js");
+const userModel = require("./model/userModel.js")
 
-// Create Express app
-const app = express();
+var app = express();
+var bodyParser=require("body-parser");
+var cookieParser=require("cookie-parser");
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
 // Add CORS to all routes and methods
-app.use(cors());
+//app.use(cors());
 
 // Enable parsing of JSON bodies
 app.use(express.json());
+
+const userroute = require('./route/user.js');
+
+//both index.js and things.js should be in same directory
+app.use('/user', userroute);
+app.listen(3001);
+// Define port
+const port = 4000;
+
+
 
 // Start listening to the port
 app.listen(port, () => {
