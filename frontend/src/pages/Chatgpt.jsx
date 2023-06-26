66ba0288c8d2f23945ecf393eb0d45819070ac47
FranciscoCheca06 <137060007+FranciscoCheca06@users.noreply.github.com>
2023-06-26 23:01:53 +0200

    Updated Chatgpt.jsx

--- frontend/src/pages/Chatgpt.jsx
+++ frontend/src/pages/Chatgpt.jsx
@@ -1,11 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react';
+import './Chatgpt.css';
 
-function chatgpt() {
+const ChatGPT = () => {
+  const [messages, setMessages] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleSendMessage = () => {
+    if (inputValue.trim() !== '') {
+      const newMessage = {
+        id: messages.length + 1,
+        content: inputValue,
+      };
+      setMessages([...messages, newMessage]);
+      setInputValue('');
+    }
+  };
+
   return (
-    <div>
-      
+    <div className="chat-container">
+      <div className="chat-header">
+        <h2>ChatGPT Chatroom</h2>
+        <p>Welcome to the ChatGPT Chatroom! Feel free to ask any questions.</p>
+      </div>
+      <div className="chat-messages">
+        {messages.map((message) => (
+          <div className="message" key={message.id}>
+            {message.content}
+          </div>
+        ))}
+      </div>
+      <div className="chat-input">
+        <input
+          type="text"
+          placeholder="Type your message..."
+          value={inputValue}
+          onChange={handleInputChange}
+        />
+        <button onClick={handleSendMessage}>Send</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default chatgpt
+export default ChatGPT;
