3a9caac7fba111f395a385585a9f4066666ffcff
dandan950 <dandan95dandan@gmail.com>
2023-07-02 10:33:13 +0200

    accountsetting

--- frontend/src/components/UserProfil/Accountsetting.jsx
+++ frontend/src/components/UserProfil/Accountsetting.jsx
@@ -1,22 +1,20 @@
 import React from 'react'
 import { TextField,Grid ,Button} from '@mui/material';
+import "./Accountsetting.css"
+import { useState} from "react";
 
 const Accountsetting = () => {
 
   let username=localStorage.getItem("userName");
-
   let useremail = localStorage.getItem("userEmail");
-  async function fetchUserData(){
-    console.log(localStorage.getItem("userName"));
-    // let userData=  await fetch("/user/userdata").then(response=>{
-    //   console.log(response);
-    //   return response.json();
-    // })
-    // .then(data=>{
-    //   console.log(data);
-    // })
+  const [values,setValues]=useState({
+    nemail:"",
+    nname:""
+  })
+  const handleSubmit=(event)=>{
+    event.preventDefault();
+   
   }
-  fetchUserData();
   return (
     <>
     <div className='accountsetting'>
@@ -24,42 +22,43 @@ const Accountsetting = () => {
 
 
     <Grid container className="form">
-          <Grid item xs={12}md={12}>
-             <p>Username:{username}</p>
-             <p>Email:{useremail}</p>
-          </Grid>
-          <Grid item xs={12} md={12}>
+          <Grid item xs={12}md={6} className='userdata'>
             <br />
-            <p>You could change your username and email:</p>
-          </Grid>
-          <Grid item xs={12} md={12}>
             <br />
-          </Grid>
-          <Grid item xs={12}md={12} className="form-group">
-                <TextField id="email" label="Email" variant="outlined" />
-          </Grid>
-          
-          <Grid item xs={12} md={12}>
-            <br />
+             <p>Your Username is:{username}</p>
+             <br />
+             <p>Your Email is:{useremail}</p>
           </Grid>
 
-          <Grid item xs={12}md={12} className="form-group">
+          <Grid container xs={12} md={6}>
+            <form action="">
+              <Grid item xs={12} md={12}>
+                <br />
+                <p>You could change your username and email:</p>
+              </Grid>
+              <Grid item xs={12} md={12}>
+                <br />
+              </Grid>
+              <Grid item xs={12}md={12} className="form-group">
+                    <TextField id="email" label="Email" variant="outlined" />
+              </Grid>
+              
+              <Grid item xs={12} md={12}>
+                <br />
+              </Grid>
+              <Grid item xs={12}md={12} className="form-group">
                 <TextField id="username" label="User name" variant="outlined" />
+              </Grid>
+              
+              
+              <Grid item xs={12} md={12}>
+                <br />
+              </Grid>
+              <Grid  item xs={12} md={12}>
+                        <Button variant="contained" color="success">Save Change</Button>
+              </Grid>
+              </form>
           </Grid>
-          
-          
-          <Grid item xs={12} md={12}>
-            <br />
-          </Grid>
-           <Grid  item xs={12} md={12}>
-                    <Button variant="contained" color="success">Save Change</Button>
-           </Grid>
-
-           <Grid item xs={12} md={12}>
-            <br />
-          </Grid>
-      
-
     </Grid>
     </div>
     </>
