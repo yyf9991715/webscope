3d957d533a1cfb9280d377366977de4dadc680d1
FranciscoCheca06 <137060007+FranciscoCheca06@users.noreply.github.com>
2023-07-03 12:18:26 +0200

    Update server.js for chat.js

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,7 @@ var app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
+
 // Add CORS to all routes and methods
 app.use(cors({
     origin: true,
@@ -22,13 +23,14 @@ app.use(express.json());
 
 const userroute = require('./route/user.js');
 const bookroute = require("./route/book.js");
+const chatroute = require("./route/chat.js");
 //both index.js and things.js should be in same directory
 app.use('/user', userroute);
-app.use('/book',bookroute);
+app.use('/book',bookroute)
+app.use('/chat', chatroute);
 // Define port
 const port = 4000;
 // Start listening to the port
 app.listen(port, () => {
     console.log("Listening on " + port + ".");
 }); 
-
