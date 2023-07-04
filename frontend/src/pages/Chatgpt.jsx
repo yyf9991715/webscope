a8dc96791fd5bc29417cb56bbe3b930dbaf79afa
FranciscoCheca06 <137060007+FranciscoCheca06@users.noreply.github.com>
2023-07-04 21:47:35 +0200

    Finished chatgpt page

--- frontend/src/pages/Chatgpt.jsx
+++ frontend/src/pages/Chatgpt.jsx
@@ -1,71 +1,80 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 import './Chatgpt.css';
 
 const ChatGpt = () => {
-  const [messages, setMessages] = useState([]);
-  const [inputValue, setInputValue] = useState('');
+    const [messages, setMessages] = useState([]);
+    const [inputValue, setInputValue] = useState('');
+    const chatMessagesRef = useRef(null);
 
-  const sendMessage = async () => {
-    if (inputValue.trim() === '') return;
+    const scrollToBottom = () => {
+        chatMessagesRef.current.scrollTop = chatMessagesRef.current.scrollHeight;
+    };
 
-    // Add user message to the chat
-    setMessages((prevMessages) => [
-      ...prevMessages,
-      { role: 'user', content: inputValue },
-    ]);
+    const sendMessage = async () => {
+        if (inputValue.trim() === '') return;
 
-    // Clear input field
-    setInputValue('');
+        // Add user message to the chat
+        setMessages((prevMessages) => [
+            ...prevMessages,
+            { role: 'user', content: inputValue },
+        ]);
 
-    try {
-      // Send user message to the server
-      const response = await axios.post('http://localhost:4000/chat', { message: inputValue });
+        // Clear input field
+        setInputValue('');
 
-      // Add ChatGPT's response to the chat
-      setMessages((prevMessages) => [
-        ...prevMessages,
-        { role: 'chatbot', content: response.data.response },
-      ]);
-    } catch (error) {
-      console.error('Error sending message:', error);
-    }
-  };
+        try {
+            // Send user message to the server
+            const response = await axios.post('http://localhost:4000/chat', { message: inputValue });
 
-  return (
-    <div className="chat-container" id="chat-container">
-      <div className="chat-header">
-        <h2>Chat with ChatGPT</h2>
-        <p>Type a message and press Enter to send</p>
-      </div>
-      <div className="chat-messages">
-        {messages.map((message, index) => (
-          <div
-            key={index}
-            className={`message ${message.role === 'user' ? 'user-message' : 'chatbot-message'}`}
-          >
-            {message.content}
-          </div>
-        ))}
-      </div>
-      <div className="chat-input">
-        <input
-          type="text"
-          placeholder="Type your message..."
-          value={inputValue}
-          onChange={(e) => setInputValue(e.target.value)}
-          onKeyPress={(e) => {
-            if (e.key === 'Enter') {
-              sendMessage();
-            }
-          }}
-        />
-        <button onClick={sendMessage}>Send</button>
-      </div>
-    </div>
-  );
+            // Add ChatGPT's response to the chat
+            setMessages((prevMessages) => [
+                ...prevMessages,
+                { role: 'chatbot', content: response.data.response },
+            ]);
+
+            scrollToBottom(); // Scroll to the bottom after adding a new message
+        } catch (error) {
+            console.error('Error sending message:', error);
+        }
+    };
+
+    useEffect(() => {
+        scrollToBottom(); // Scroll to the bottom when component mounts
+    }, [messages]); // Scroll to the bottom when messages state updates
+
+    return (
+        <div className="chat-container" id="chat-container">
+            <div className="chat-header">
+                <h2>Chat with ChatGPT</h2>
+                <p>Type a message and press Enter to send</p>
+            </div>
+            <div className="chat-messages" ref={chatMessagesRef}>
+                {messages.map((message, index) => (
+                    <div
+                        key={index}
+                        className={`message ${message.role === 'user' ? 'user' : 'assistant'}`}
+                    >
+                        {message.content}
+                    </div>
+                ))}
+            </div>
+            <div className="chat-input">
+                <input
+                    type="text"
+                    placeholder="Type your message..."
+                    value={inputValue}
+                    onChange={(e) => setInputValue(e.target.value)}
+                    onKeyPress={(e) => {
+                        if (e.key === 'Enter') {
+                            sendMessage();
+                        }
+                    }}
+                />
+                <button onClick={sendMessage}>Send</button>
+            </div>
+        </div>
+    );
 };
 
 export default ChatGpt;
-
-
