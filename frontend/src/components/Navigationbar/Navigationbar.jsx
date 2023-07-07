0d59f9b5dc63c14298a6e63b9875687815e2bfef
FranciscoCheca06 <137060007+FranciscoCheca06@users.noreply.github.com>
2023-07-07 18:38:17 +0200

    Navigation bar fix

--- frontend/src/components/Navigationbar/Navigationbar.jsx
+++ frontend/src/components/Navigationbar/Navigationbar.jsx
@@ -1,47 +1,48 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import logo from "../../assets/e114b11cc9214759b53d1ea0a37a4ca1.png";
 import "./Navigationbar.css";
 
-const Navigationbar = ({ isLoggedIn, onLogin, onLogout }) => {
-  return (
-    <nav className="nav">
-      <Link to="/" className="site-title">
-        <img src={logo} alt="eLearningHelper Logo" />
-      </Link>
+const Navigationbar = () => {
+    const navigate = useNavigate();
 
-      <ul>
-        {isLoggedIn && (
-          <li>
-            <Link to="/search">Search</Link>
-          </li>
-        )}
-        {isLoggedIn ? (
-          <>
-            <li className="login-button">
-              <Link to="/login">Login</Link>
-            </li>
-            <li className="signup-button">
-              <Link to="/signup">Sign up</Link>
-            </li>
-          </>
-        ) : (
-          <>
-            <li>
-              <li className="user-profile-button">
-                <Link to="/user/accountsetting">User Profile</Link>
-              </li>
-            </li>
-            <li className="login-button">
-              <button className="logout-button" onClick={onLogout}>
-                Logout
-              </button>
-            </li>
-          </>
-        )}
-      </ul>
-    </nav>
-  );
+    const handleLogout = () => {
+        localStorage.clear();
+        navigate("/");
+    };
+
+    return (
+        <nav className="nav">
+            <Link to="/" className="site-title">
+                <img src={logo} alt="eLearningHelper Logo" />
+            </Link>
+
+            <ul>
+                {!localStorage.getItem("userid") ? (
+                    <>
+                        <li className="login-button">
+                            <Link to="/login">Login</Link>
+                        </li>
+                        <li className="signup-button">
+                            <Link to="/signup">Sign up</Link>
+                        </li>
+                    </>
+                ) : (
+                    <>
+                        <li>
+                            <Link to="/search">Search</Link>
+                        </li>
+                        <li className="user-profile-button">
+                            <Link to="/user/accountsetting">User Profile</Link>
+                        </li>
+                        <li className="logout-button" onClick={handleLogout}>
+                            Logout
+                        </li>
+                    </>
+                )}
+            </ul>
+        </nav>
+    );
 };
 
 export default Navigationbar;
