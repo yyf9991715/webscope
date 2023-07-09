5a63250e788d9936d0823960ac99b1350ec5f09b
dandan950 <dandan95dandan@gmail.com>
2023-07-09 13:06:22 +0200

    fix navigationbar logout in home page

--- frontend/src/components/Navigationbar/Navigationbar.jsx
+++ frontend/src/components/Navigationbar/Navigationbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
-import logo from "../../assets/e114b11cc9214759b53d1ea0a37a4ca1.png";
+import logo from "../../assets/98ee5cf8b55f49b8922d57a18e771827-3.png";
 import "./Navigationbar.css";
 
 const Navigationbar = () => {
@@ -8,6 +8,7 @@ const Navigationbar = () => {
 
     const handleLogout = () => {
         localStorage.clear();
+        alert("log out!")
         navigate("/");
     };
 
